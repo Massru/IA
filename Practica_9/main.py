843c0898b3ddd0a134ac73f673c74f28f6ac3f4e
Angel Massanet Ruiz <angelmassruiz@gmail.com>
2023-12-14 14:42:05 +0100

    FIN P9

--- Practica_9/main.py
+++ Practica_9/main.py
@@ -1,8 +1,9 @@
 from minimaxAlum import *
 
 ganador = 0
-jugador = int(input("Introduzca el 1er jugador: 1 AI, 2 Tú "))
+jugador = int(input("Introduzca el 1er jugador: 1 AI, 2 Tú: "))
 modo = input("¿Desea jugar con el teclado numérico? y/n: ")
+busqueda = input("Tipo de búsqueda: 1 Minimax 2 Poda_ab: ")
 
 if jugador != 1:
     jugador = -1
@@ -10,7 +11,10 @@ if jugador != 1:
 juego = nodoInicial()
 while juego.vacias > 0 and not ganador:
     if jugador == 1:
-        juego = poda_ab(juego)
+        if busqueda == "1":
+            juego = minimax(juego)
+        else:
+            juego = poda_ab(juego)
     else:
         juego = jugadaAdversario(juego, modo)
     print(juego)
